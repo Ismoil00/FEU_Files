904ab4f5134b3fe09c131870fffde3ac47b64220
Muhammad <khojaqulovm17@gmail.com>
2026-03-10 16:11:18 +0500

    Add upsert_ledger function and related SQL queries for accounting operations; include comments for frontend and backend integration.

--- helpers/SCALING-CODES.sql
+++ helpers/SCALING-CODES.sql
@@ -18,8 +18,6 @@
 
 -- "upsert_cash_receipt_order"
 
--- "upsert_manual_operations"
-
 -- "upsert_payment_order_incoming"
 
 -- "upsert_payment_order_outgoing"
@@ -33,3 +31,86 @@
 -- "upsert_warehouse_services"
 
 -- "create_pensioners_payroll"
+
+----------------------------------------------------------
+
+/*
+	FRONT:
+		- SearchPage main-department-id
+		- CreatePage main-department-id
+	BACKEND:
+		- UPSERT
+		- GET
+		- Ledger
+		- FastAPI
+*/
+
+
+
+select * from accounting.ledger
+where id > 2299
+order by id
+
+
+select parent_id from commons.department
+
+
+CREATE OR REPLACE FUNCTION accounting.upsert_ledger (
+	_financing accounting.budget_distribution_type,
+	_debit integer,
+	_credit integer,
+	_amount numeric,
+	_main_department_id integer,
+	_contract_id bigint DEFAULT NULL::bigint,
+	_staff_id bigint DEFAULT NULL::bigint,
+	ledger_id bigint DEFAULT NULL::bigint,
+	_created_date date default null::date
+);
+
+
+
+/* we fill ledger with the accountingentry */
+SELECT accounting.upsert_ledger(
+	_financing,
+	_debit,
+	_credit,
+	round(_amount * _quantity, 2),
+	_main_department_id,
+	_contract_id,
+	null,
+	_ledger_id,
+	_created_date
+) INTO _ledger_id;
+
+
+
+select * from accounting.payment_order_incoming order by id
+
+select * from accounting.payment_order_outgoing order by id
+
+select * from accounting.cash_payment_order order by id
+
+select * from accounting.cash_receipt_order order by id
+
+select * from accounting.advance_report_prochee order by id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
