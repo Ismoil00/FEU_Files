2a9cdf8313b01f74746c5209100c1618fc2fc0d8
Ismoil Sharifov <sharipovismoil01@gmail.com>
2026-03-14 01:31:43 +0500

    1

--- helpers/pension.sql
+++ helpers/pension.sql
@@ -1,16 +1,1 @@
 
-/* PENSION SECTIONS THAT MAIN-DEPARTMENT-ID MUST BE INTEGRATED */
-
--- pension.get_a_pensioner_for_type_convertion ()
-
--- pension.change_pensioner_type
-
--- pension.create_new_recipient_for_died_pensioner ()
-
-
-/* PAYROLL */
--- pension.get_pensioners_this_month_active_payroll
-
--- pension.create_pensioners_payroll ++++++++++++++
-
--- pension.close_pensioners_payroll
