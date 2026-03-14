c7fe16b42910758d5f1b44d2772d6b41ab2996c4
Ismoil Sharifov <sharipovismoil01@gmail.com>
2026-03-14 16:14:39 +0500

    Remove deprecated upsert_ledger functions and related SQL queries from ledger.sql and SCALING-CODES.sql; update comments for clarity on required table modifications.

--- helpers/SCALING-CODES.sql
+++ helpers/SCALING-CODES.sql
@@ -1,67 +1,16 @@
--- "generate_ledger_for_payroll_sheet"
 
--- "handle_payroll_sheet_from_bank_and_cash"
-
--- "process_retention"
-
-----------------------------------------------------------
-
-/*
-	FRONT:
-		- SearchPage main-department-id
-		- CreatePage main-department-id
-	BACKEND:
-		- UPSERT
-		- GET
-		- Ledger
-		- FastAPI
+/* 
+	The following TABLES must be modified:
+		- payments.payroll_sheet_line
+		- payments.payroll_sheet
+	After its modification the following functions must be modified as well:
+		- 'handle_payroll_sheet_from_bank_and_cash';
+		- 'generate_ledger_for_payroll_sheet';
+		- 'process_retention';
 */
 
+---------------------------------------------------------
 
-
-
-
-select parent_id from commons.department
-
-
-CREATE OR REPLACE FUNCTION accounting.upsert_ledger (
-	_financing accounting.budget_distribution_type,
-	_debit integer,
-	_credit integer,
-	_amount numeric,
-	_main_department_id integer,
-	_contract_id bigint DEFAULT NULL::bigint,
-	_staff_id bigint DEFAULT NULL::bigint,
-	ledger_id bigint DEFAULT NULL::bigint,
-	_created_date date default null::date
-);
-
-
-
-/* we fill ledger with the accountingentry */
-SELECT accounting.upsert_ledger(
-	_financing,
-	_debit,
-	_credit,
-	round(_amount * _quantity, 2),
-	_main_department_id,
-	_contract_id,
-	null,
-	_ledger_id,
-	_created_date
-) INTO _ledger_id;
-
-
-
-select * from accounting.ledger
-where id > 2332
-order by id
-
-
-
-select * from accounting.product_transfer
--- where id > 716
-order by id
 
 
 
